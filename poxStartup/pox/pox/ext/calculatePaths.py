b9641943b51373120683a3c70a484284cb218522
zoebohn <zbohn2@stanford.edu>
2018-05-02 20:09:20 +0000

    figure 9 script should work now

--- poxStartup/pox/pox/ext/calculatePaths.py
+++ poxStartup/pox/pox/ext/calculatePaths.py
@@ -10,8 +10,8 @@ def main():
     ECMP(graph)
 
 def ECMP(graph):
-    source = 0
-    target = 8
+    ECMP = False#True
+    ECMP_8 = False#True
     link_count = {}
     for node_i in graph.nodes():
         for node_j in graph.nodes():
@@ -24,17 +24,41 @@ def ECMP(graph):
         node_j = node_i + 1
         if (node_j == len(graph.nodes())):
             node_j = 1
-        shortest_paths = [p for p in nx.all_shortest_paths(graph, node_i, node_j)]
         #ECMP 8
-        shortest_paths = shortest_paths[:8]
+        if (ECMP):
+            shortest_paths = [p for p in nx.all_shortest_paths(graph, node_i, node_j)]
+            if (ECMP_8):
+                shortest_paths = shortest_paths[:8]
+            else:
+                shortest_paths = shortest_paths[:64]
+        else:
+            shortest_paths = [p for p in nx.shortest_simple_paths(graph, node_i, node_j)]
+            shortest_paths = shortest_paths[:8]
+        
         for path in shortest_paths:
             i = 0
             while i + 1 < len(path):
                 edge = (min(path[i], path[i + 1]), max(path[i], path[i + 1]))
                 link_count[edge] += 1
                 i += 1
-    for key in link_count: 
-        print "Edge: " + str(key[0]) + "-" + str(key[1]) + ". Appears on " + str(link_count[key]) + " paths."
+
+    highest_count = max(link_count.items(), key=operator.itemgetter(1))[1]
+    print highest_count
+    rank_to_count = [0] * (highest_count + 1) 
+    for key in link_count:
+        count = link_count[key]
+        for i in range(0, count + 1):
+            print i
+            print count + 1
+            rank_to_count[i] += 1
+
+    links_so_far = 0;
+    for i in range(0, len(rank_to_count)):
+        start_range = links_so_far
+        end_range = rank_to_count[i] + links_so_far
+        links_so_far += rank_to_count[i]
+        print str(end_range) + "," str(i);
+        
 
 """
 def YenKSP(graph, source, target, K):
