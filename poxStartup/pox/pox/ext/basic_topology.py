074c3cd8c5bf40ed959ae023ae54abcbfac20167
Emma Dauterman <evd2014@gmail.com>
2018-05-03 05:35:40 +0000

    adding missing files

---
+++ poxStartup/pox/pox/ext/basic_topology.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import networkx as nx
+import json
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
+from mininet.cli import CLI, info
+from mininet.log import setLogLevel
+sys.path.append("../../")
+from subprocess import Popen
+from mininet.node import Controller, RemoteController, Node
+from mininet.link import Link, Intf
+#from time import sleep, time
+
+def aggNet():
+
+	CONTROLLER_IP='127.0.0.1'
+
+	net = Mininet( topo=None,
+               	build=False)
+
+	net.addController( 'c0',
+                  	controller=RemoteController,
+                  	ip=CONTROLLER_IP,
+                  	port=6633)
+
+	h1 = net.addHost( 'h1', ip='0.0.0.0' )
+	h2 = net.addHost( 'h2', ip='0.0.0.0' )
+	h3 = net.addHost( 'h3', ip='0.0.0.0' )
+	h4 = net.addHost( 'h4', ip='0.0.0.0' )
+	s1 = net.addSwitch( 's1' )
+	s2 = net.addSwitch( 's2' )
+	s3 = net.addSwitch( 's3' )
+
+	net.addLink( s1, s2 )
+	net.addLink( s2, s3 )
+
+	net.addLink( h1, s1 )
+	net.addLink( h2, s1 )
+	net.addLink( h3, s3 )
+	net.addLink( h4, s3 )
+
+	net.start()
+	CLI( net )
+	net.stop()
+
+if __name__ == '__main__':
+	setLogLevel( 'info' )
+	aggNet()
