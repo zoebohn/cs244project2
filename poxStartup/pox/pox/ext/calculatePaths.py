51b60512f484258e15b181e0f3a2f06efbc668b7
zoebohn <zbohn2@stanford.edu>
2018-05-03 05:08:30 +0000

    added Figure 9 files

--- poxStartup/pox/pox/ext/calculatePaths.py
+++ poxStartup/pox/pox/ext/calculatePaths.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 def main():
     data = None
-    with open('generated_rrg', 'r') as infile:
+    with open('figure9_rrg', 'r') as infile:
         data = json.load(infile)
     graph = nx.readwrite.node_link_graph(data)
     x, y = generate_data(graph, True, True)
@@ -21,7 +21,21 @@ def main():
     plt.legend()
     plt.xlabel("Rank of Link")
     plt.ylabel("# Distinct Paths Link is On")
+    plt.xlim(xmin=0, xmax=3000)
+    plt.ylim(ymin=0, ymax=25)
     plt.savefig('Figure9.png')
+
+def bfs_paths(graph, start, goal):
+    queue = [(start, [start])]
+    limit = 0
+    while queue and limit < 7:
+        (vertex, path) = queue.pop(0)
+	for next in set(graph.neighbors(vertex)) - set(path):
+            if next == goal:
+		limit += 1
+		yield path + [next]
+            else:
+                queue.append((next, path + [next]))
 
 def generate_data(graph, ecmp, ecmp_8):
     ECMP = ecmp
@@ -31,63 +45,37 @@ def generate_data(graph, ecmp, ecmp_8):
         link_count[edge] = 0
 
     for node_i in graph.nodes():
-        #print "working on node: " + str(node_i)
         node_j = node_i + 1
         if (node_j == len(graph.nodes())):
             node_j = 1
         #ECMP 8
         if (ECMP):
-            shortest_paths = [p for p in nx.all_shortest_paths(graph, node_i, node_j)]
             if (ECMP_8):
-                shortest_paths = shortest_paths[:8]
+                shortest_paths = list(islice(nx.all_shortest_paths(graph, node_i, node_j), 7))
             else:
-                shortest_paths = shortest_paths[:64]
+                shortest_paths = list(islice(nx.all_shortest_paths(graph, node_i, node_j), 63))
         else:
-            shortest_paths = list(islice(nx.shortest_simple_paths(graph, node_i, node_j), 7))
+            	shortest_paths = list(bfs_paths(graph, node_i, node_j))[:7]
+		#shortest_paths = list(islice(nx.shortest_simple_paths(graph, node_i, node_j), 4))
         
         for path in shortest_paths:
             i = 0
-            while i + 1 < len(path):
-                edge = (min(path[i], path[i + 1]), max(path[i], path[i + 1]))
+            while (i + 1) < len(path):
+                edge = (path[i], path[i + 1])
                 link_count[edge] += 1
                 i += 1
 
+    
     highest_count = max(link_count.items(), key=operator.itemgetter(1))[1]
     rank_to_count = [0] * (highest_count + 1) 
     for key in link_count:
         count = link_count[key]
         for i in range(count, len(rank_to_count)):
             rank_to_count[i] += 1
-
     x = []
     y = []
-    links_so_far = 0;
     for i in range(0, len(rank_to_count)):
-        start_range = links_so_far
-        end_range = rank_to_count[i]# + links_so_far
-        links_so_far += rank_to_count[i]
+        end_range = rank_to_count[i]
         x.append(end_range)
         y.append(i)
     return x, y
-"""
-def YenKSP(graph, source, target, K):
-    l, shortest_path = nx.single_source_dijkstra(graph, source, target)
-    A = []
-    A.append(shortest_path)
-
-    B = []
-
-    for k in range(1, K):
-        for i in range(0, size(A[k - 1]) - 1):
-            spurNode = A[k-1][i]
-            rootPath = A[k-1][:i]
-
-            removed_edges = []; removed_root_edges = []; removed_root_nodes = [];
-
-            for path in A:
-                if rootPath == p[:i]
-
-"""
-
-if __name__ == "__main__":
-    main()
